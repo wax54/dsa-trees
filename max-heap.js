43ff32fda1654a35a7b9ff297aacd1a95ffd6ea6
sam <wax654@gmail.com>
2021-08-14 16:19:42 -0700

    Print Map is looking pretty good

--- max-heap.js
+++ max-heap.js
@@ -1,11 +1,43 @@
 
-class maxHeapNode {
+class MaxHeapNode {
     constructor(val, left = null, right = null) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
-
+    //EX: returns 
+    //  [childL, childR, 
+    //     [gcLL, gclR,
+    //         [null, ggcLLR, null || empty, [null, null]],
+    //         [null, null]
+    //     ],
+    //     [gcRL, gcRR,
+    //         [null, null], 
+    //         [null, null]
+    //     ]
+    //  ]
+    getMap() {
+        //Base Case
+        if(this.isLeaf()) return([null,null]);
+        //Normal Case
+        let result = [];
+        if(this.left){
+            result[0] = this.left.val;
+            result[2] = this.left.getMap();
+        } else {
+            result[0] = null;
+            result[2] = null;
+        }
+        if (this.right) {
+            result[1] = this.right.val;
+            result[3] = this.right.getMap();
+        } else {
+            result[1] = null;
+            result[3] = null;
+        }
+        return result;
+    }
+    
     isLeaf() {
         return !(this.left || this.right)
     }
@@ -19,21 +51,54 @@ class maxHeapNode {
         } else if(!this.right) {
             this.right = node;
         } else {
-            this.left.append(node);
+            this.left = this.left.append(node);
         }
         return this;
     }
 }
 
 
-class maxHeap {
+class MaxHeap {
     constructor(head=null){
         this.head = head;
         this.size;
     }
 
+    getMap() {
+        if(this.head)
+            return [this.head.val, this.head.getMap()];
+        else
+            return null;
+    }
+    printMap() {
+        const _printMapRecurse = (arr) => {
+            if (arr[0] === null && arr[1] === null) 
+                return "null            null";
+            
+            let currLine = `${arr[0]}          ${arr[1]}\n`;
+            currLine +=    "  /  \\                 /  \\\n";
+
+            if(arr[2] === null){
+                currLine += "                ";
+            } else {
+                currLine += _printMapRecurse(arr[2]);
+            } if (arr[3] === null) {
+                currLine += "                ";
+            } else {
+                currLine += _printMapRecurse(arr[3]);
+            }
+            currLine += "\n";
+            return currLine;
+        }
+
+        const map = this.getMap();
+        console.log('     ', map[0], '-    ');
+        console.log("     /  \\  ");
+        console.log(_printMapRecurse(map[1]))
+
+    }
     append(val) {
-        const node = maxHeapNode(val);
+        const node = new MaxHeapNode(val);
         if(this.head === null) {
             this.head = node;
         } else {
@@ -66,4 +131,5 @@ class maxHeap {
         return returnVal
     }
 }
-const heapHead = new maxHeapNode(100);
+const heapHead = new MaxHeapNode(100);
+module.exports = { MaxHeap, MaxHeapNode };
