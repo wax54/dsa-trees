07c876eb10878896a9986db613d380867af3b463
sam <wax654@gmail.com>
2021-08-16 11:39:23 -0700

    Made some advancements

--- max-heap.js
+++ max-heap.js
@@ -46,12 +46,12 @@ class MaxHeapNode {
         if (node.val > this.val ) {
             node.left = this;
             return node;
+        } else if (!this.right) {
+            this.right = node;
         } else if(!this.left) {
             this.left = node;
-        } else if(!this.right) {
-            this.right = node;
         } else {
-            this.left = this.left.append(node);
+            this.right = this.right.append(node);
         }
         return this;
     }
@@ -62,6 +62,64 @@ class MaxHeap {
     constructor(head=null){
         this.head = head;
         this.size;
+    }   
+    /**
+     * #GOAL
+     * ifmap.length == 2
+     *   500
+     *  /  \
+     * n    n
+     * 
+     *OR if map.length == 4
+     *          500
+     *          /    \
+     *      107        185
+     *      /  \      /   \
+     *    106   74   162   n
+     *    /  \ / \  / \ / \
+     *   n   n n n  n n n n
+     */
+
+    printMoreUsefulMap() {
+        const map = this.getMoreUsefulMap();
+        for(i in map) {
+            if(typeof map[i] !== "array"){
+                console.log(" ".repeat(3 * (i - map.length))+map[i]);
+                console.log(" ".repeat(3 * (i - map.length))+"");
+
+            }else {
+                printArray(map[i], i);
+            }
+        }
+
+    }
+    //Goal [1, [1, 2], [null, 1, 2, 3], [null, null, 1, 2, 8, null, 1, null, null]]
+    getMoreUsefulMap() {
+        const finalResult = [];
+        let result = finalResult;
+        const queue = [this.head, "LINEBREAK"];
+        let currNode;
+
+        while(queue.length) {
+            currNode = queue.shift();
+            if(currNode === "LINEBREAK") {
+                const temp = [];
+                finalResult.push(temp);
+                result = temp;
+                /** when we hit a line break, we have added the children 
+                 * of all the nodes from this line to the queue, it's time to add another line break on top 
+                 * (if nothings in the queue, we have no more lines)
+                 * */
+                if(queue.length)
+                    queue.push("LINEBREAK");
+                continue;
+            }
+            result.push(currNode ? currNode.val : null);
+            if(currNode){
+                queue.push(currNode.left, currNode.right);
+            }
+        }
+        return finalResult;
     }
 
     getMap() {
@@ -73,10 +131,10 @@ class MaxHeap {
     printMap() {
         const _printMapRecurse = (arr) => {
             if (arr[0] === null && arr[1] === null) 
-                return "null            null";
+                return "null null";
             
-            let currLine = `${arr[0]}          ${arr[1]}\n`;
-            currLine +=    "  /  \\                 /  \\\n";
+            let currLine = `${arr[0] || "null"}   ${arr[1]|| "null"}\n`;
+            currLine +=    "/  \\     /  \\\n";
 
             if(arr[2] === null){
                 currLine += "                ";
@@ -92,13 +150,14 @@ class MaxHeap {
         }
 
         const map = this.getMap();
-        console.log('     ', map[0], '-    ');
-        console.log("     /  \\  ");
+        console.log(' ', map[0], '');
+        console.log("  /   \\  ");
         console.log(_printMapRecurse(map[1]))
 
     }
     append(val) {
         const node = new MaxHeapNode(val);
+        this.size++;
         if(this.head === null) {
             this.head = node;
         } else {
@@ -110,7 +169,7 @@ class MaxHeap {
     pop() {
         if(this.head === null) throw Error("NO ITEMS IN HEAP!");
         const returnVal = this.head.val;
-
+        this.size--;
         if(this.head.isLeaf()){
             this.head = null;
         }else if(!this.head.left) {
@@ -132,4 +191,15 @@ class MaxHeap {
     }
 }
 const heapHead = new MaxHeapNode(100);
-module.exports = { MaxHeap, MaxHeapNode };
+const tree = new MaxHeap(heapHead);
+tree.append(100);
+tree.append(10);
+tree.append(1);
+tree.append(5);
+tree.append(500);
+tree.append(105);
+tree.append(-100);
+tree.append(-00);
+
+
+module.exports = { MaxHeap, MaxHeapNode, tree };
